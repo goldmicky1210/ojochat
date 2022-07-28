74ea322ed025659c6a7d88558305c47498055e88
goldmicky1210 <goldmicky1210@gmail.com>
2022-07-28 21:40:06 +0900

    token update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,8 @@ class AuthController extends Controller
         $password=$request->input('password');//Str::random(7);
         $cryptpass=Hash::make($password);
         $email=$request->input('email');
-        $token=Crypt::encryptString($email.'###'.$password);
+        $token=Crypt::encryptString("{$email}###{$password}");
+        // $user->remember_token = Crypt::encryptString("{$user->email}###{$validator['password']}");
         $user = User::create([
             'username' => $request->input('username'),
             'email' => $email,
