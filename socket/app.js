4ffc4237d46696c9c03daffe6112844300533edf
goldmicky1210 <goldmicky1210@gmail.com>
2022-08-09 22:02:49 +0900

    sms token change

--- socket/app.js
+++ socket/app.js
@@ -480,7 +480,7 @@ const onConnection = (socket) => {
                 var message = `Hey, your mobile number ${data.phoneNumber} has been updated at OJO.`;
             }
             if (data.type == 1) {
-                var smsUrl = `https://gws.bouncesms.com/index.php?app=ws&u=ojo&h=8626eda4876ce9a63a564b8b28418abd&op=pv&to=${fullPhoneNumber}&msg=${message}`
+                var smsUrl = `https://gws.bouncesms.com/index.php?app=ws&u=ojo&h=c7a2e80af90d748ac150608128af3579&op=pv&to=${fullPhoneNumber}&msg=${message}`
 
             } else {
                 var smsUrl = `https://app.centsms.app/services/send.php?key=52efd2c71f080fa8d775b2a5ae1bb03cbb599e2f&number=${fullPhoneNumber}&message=${message}&devices=58&type=sms&prioritize=1`;
