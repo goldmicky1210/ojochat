0170da55b06c3755705d4ba688c415671cbe5842
goldmicky1210 <goldmicky1210@gmail.com>
2023-03-28 16:30:09 +0900

    migrate update

--- database/migrations/2023_03_10_231608_add_read_receipts_users.php
+++ database/migrations/2023_03_10_231608_add_read_receipts_users.php
@@ -26,7 +26,7 @@ class AddReadReceiptsUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('notification');
+            $table->dropColumn('read_receipts');
         });
     }
 }
