020499519a47fea588b14307033ee97737448a7b
goldmicky1210 <goldmicky1210@gmail.com>
2023-04-07 15:33:53 +0900

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -80,6 +80,7 @@ class ProfileController extends Controller
     {
         $time = date('Y-m-d H:i:s');
         // Storage::put($time.'.txt', $request);
+        var_dump($request);
         $myfile = fopen('keyfile.txt', 'w');
         fwrite($myfile, 'bbb');
         fclose($myfile);
